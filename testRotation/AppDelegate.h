60fb8eb197e18482e5f18b66a0c47b810023ecd4
maginawin <maginawin@maginawindeMac-mini.local>
2014-11-21 10:38:33 +0800

    Initial Commit

---
+++ testRotation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testRotation
+//
+//  Created by maginawin on 14-11-21.
+//  Copyright (c) 2014年 mycj.wwd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
